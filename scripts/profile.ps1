0c6aaac84a5d40322807fb958b25430bf3671d57
Brandon Young <btyoung2k@gmail.com>
2020-10-08 14:57:24 -0400

    Updates: -Finished new project worflow -changed command name to prj -changed new command to generate -added no-command option

--- scripts/profile.ps1
+++ scripts/profile.ps1
@@ -2,17 +2,20 @@ Import-Module posh-git
 Import-Module oh-my-posh
 Set-Theme Paradox
 Set-Alias code code-insiders
-Function project() {
+Set-Alias vi vim
+Function prj() {
     Param(
         [Parameter(Mandatory=$true,Position=0)]
         [string]$command,
         [Parameter(Mandatory=$true,Position=1,ParameterSetName='new')]
         [string]$projectName,
         [Parameter(ParameterSetName='new')]
-        [string]$path
+        [string]$path,
+        [Parameter(ParameterSetName='new')]
+        [switch]$private
     )
-    if($command = "new"){
-        Write-Host 'Creating' $projectName '...'
+    if($command -eq "generate"){
+        Write-Host 'Creating' $projectName '...' -ForegroundColor green
         if($path -ne ('')){
             Write-Host $path
             Set-Location $path
@@ -20,7 +23,18 @@ Function project() {
         else{
             Set-Location '~/Sync/Git'
         }
-        mkdir $projectName
+        if($private){
+            gh repo create $projectName -y --private
+        }
+        else{
+            gh repo create $projectName -y --public
+        }
         Set-Location $projectName
+        New-Item README.md
+        git add .
+        git commit -m "initial commit"
+        git push --set-upstream origin master
     }
+else{get-command prj -get-syntax}
 }
+Clear-Host
