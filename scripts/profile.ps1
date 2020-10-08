6b78206635db50f7e481f0c4571234cee3ca5fb4
Brandon Young <btyoung2k@gmail.com>
2020-10-08 10:46:43 -0400

    Initial work on project automation. -setting up new command -adding cd commmands

---
+++ scripts/profile.ps1
@@ -0,0 +1,25 @@
+Import-Module posh-git
+Import-Module oh-my-posh
+Set-Theme Paradox
+Set-Alias code code-insiders
+Function project() {
+    Param(
+        [Parameter(Mandatory=$true,Position=0)]
+        [string]$command,
+        [Parameter(Mandatory=$true,Position=1,ParameterSetName='new')]
+        [string]$projectName,
+        [Parameter(ParameterSetName='new')]
+        [string]$path
+    )
+    if($command = "new"){
+        Write-Host 'Creating' $projectName '...'
+        if($path -ne ('')){
+            Write-Host $path
+            Set-Location $path
+        }
+        else{
+            Set-Location 'C:/users/'$env:USERNAME'/Sync/Git'
+        }
+
+    }
+}
